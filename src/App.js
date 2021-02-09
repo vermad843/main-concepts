bd08f126f60f49d34456fde732b7f82f22d48d15
vermad843 <vishnuverma7524@gmail.com>
2021-02-09 13:50:41 +0530

    forwardRef

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.css';
+import File from './componentsRef/Refs'
+
+function App() {
+  return (
+    <div>
+       <File/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentsRef/Refs.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import FRParentInput from './FRParentInput';
+
+
+
+class refs extends Component {
+    render() {
+        return (
+           <div>
+               <FRParentInput/>
+           </div>   
+        );
+    }
+}
+
+export default refs;
